33aec7c63d5f61e5c7f4dfb3908bbfb6c344d35f
sunhz <mymailcache@163.com>
2022-12-19 16:14:21 +0800

    添加条件判断是否设置请求头

--- src/utils/request.js
+++ src/utils/request.js
@@ -12,12 +12,16 @@ module.exports = (vm) => {
   uni.$u.http.interceptors.request.use((config) => { // 可使用async await 做异步操作
     // 初始化请求拦截器时，会执行此方法，此时data为undefined，赋予默认{}
     config.data = config.data || {}
-    config.header.Authorization = 'Bearer ' + getTokenSync()
+    // 看情况添加token头
+    const token = getTokenSync()
+    if (token) {
+      // console.log('Bearer', token)
+      config.header.Authorization = 'Bearer ' + token
+    }
     // 根据custom参数中配置的是否需要token，添加对应的请求头
     // if(config?.custom?.auth) {
     //     // 可以在此通过vm引用vuex中的变量，具体值在vm.$store.state中
     //     // config.header.token = vm.$store.state.userInfo.token
-    //     config.header.Authorization = 'Bear 11112222'
     // }
     return config
   }, config => { // 可使用async await 做异步操作
